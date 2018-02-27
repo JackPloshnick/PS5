fd642a8e047fa3e6160b9eb20af31b951efb5eff
JackPloshnick <36051309+JackPloshnick@users.noreply.github.com>
2018-02-27 15:33:57 -0600

    Starting to make trapezoid function

---
+++ Trapezoid.R
@@ -0,0 +1,14 @@
+###figuring out trapezoid
+
+#equations is
+#  y= -x^2 +5 
+
+x<- c(1,2,3,4,5)
+y<- c(4,1,-4,-11,-20)
+
+StartToEnd<- c(2,4)
+
+trap<- function(x,y,StartToEnd){
+  a= x[StartToEnd[1]]
+  
+}
