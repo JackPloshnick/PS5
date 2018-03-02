60a098bc0ea4b68081fa2a0593e9f78bc2779e06
JackPloshnick <36051309+JackPloshnick@users.noreply.github.com>
2018-03-01 21:16:09 -0600

    Working trap and Simpson function

--- Trapezoid.R
+++ Trapezoid.R
@@ -1,12 +1,12 @@
 ###figuring out trapezoid
 
 #equations is
-#  y= -x^2 +5 
+#  y= x^2
 
-x<- c(1:4)
-y<- c(1,4,9,16)
+x<- c(1,2,3,4,5)
+y<- c(1,4,9,16,25)
 
-StartToEnd<- c(1:4)
+StartToEnd<- c(1:5)
 
 trap<- function(x,y,StartToEnd){
   a= x[StartToEnd[1]]
@@ -18,7 +18,7 @@ trap<- function(x,y,StartToEnd){
   last_vlaue= tail(y, n=1)
   middle_vlaues= 2* y[2:(length(x)-1)]
   
-  answer= (h/2)* sum((first_value+ middle_vlaues+ last_vlaue))
+  answer= (h/2) * sum(first_value, last_vlaue, middle_vlaues)
   
   print(answer)
   
@@ -36,16 +36,17 @@ simpsons <- function(x,y,StartToEnd){
   
   first_value= y[1]
   last_vlaue= tail(y, n=1)
-  middle_vlaues= y[2:(length(x)-1)]
+  second_to_last= 4* head(tail(y, n=2), n=1)
+  middle_vlaues= y[2:(length(x)-2)]
   
   odd_middles= 4*middle_vlaues[seq(1,length(middle_vlaues),2)]
   
   even_middles= 2*middle_vlaues[seq(0,length(middle_vlaues),2)]
 
   
-  answer= (h/3)* sum((first_value+ odd_middles+even_middles+ last_vlaue))
+  answer= (h/3)* sum(first_value, odd_middles, even_middles,second_to_last, last_vlaue)
   
-  return(answer)
+  print(answer)
 }
 
 simpsons(x,y,StartToEnd)
